f93506c22d9739941db4e80000bad4981e6f8c3c
Ivan Carro Rojo <Ivan Carro Rojo>
2017-06-01 00:02:59 +0200

    Initial commit

---
+++ src/main/java/com/batch/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
